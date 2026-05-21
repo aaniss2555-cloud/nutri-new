de783a286f97a1f9c362c8557c3c393eb26b2def
Abdouyyt <abdoumdcsc@gmail.com>
2026-05-21 02:17:15 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,13 +114,17 @@ CORS_ALLOWED_ORIGINS = [
     "https://www.firsthostnutri33.vercel.app", 
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "abdouandoudms@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+ANYMAIL = {
+    "BREVO_API_KEY": os.environ.get("BREVO_API_KEY"),
+}
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER", "abdouandoudms@gmail.com")
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 465
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "abdouandoudms@gmail.com")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@nutritionapp.com")
 FRONTEND_URL = "http://localhost:5173"
 AI_SERVICE_PREDICT_URL = os.getenv(
