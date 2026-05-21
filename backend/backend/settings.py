1b88dd49dfb7608a5ae665c4c87d0c4c7e27c7f7
Abdouyyt <abdoumdcsc@gmail.com>
2026-05-21 01:28:58 +0100

    Update email settings for SMTP configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,8 +106,9 @@ CORS_ALLOWED_ORIGINS = [
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "abdouandoudms@gmail.com")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@nutritionapp.com")
