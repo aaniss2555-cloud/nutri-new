81a4a7d81e8b33714f9d7d8835c3934607419400
Abdouyyt <abdoumdcsc@gmail.com>
2026-05-21 02:28:51 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,13 +119,15 @@ ANYMAIL = {
     "BREVO_API_KEY": os.environ.get("BREVO_API_KEY"),
 }
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER", "abdouandoudms@gmail.com")
+# ضع هنا الإيميل الحقيقي الذي فتحت به حساب Brevo (مثلاً abdouandoudms@gmail.com)
+DEFAULT_FROM_EMAIL = "abdouandoudms@gmail.com"
 # EMAIL_HOST = "smtp.gmail.com"
 # EMAIL_PORT = 465
 # EMAIL_USE_TLS = False
 # EMAIL_USE_SSL = True
 # EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "abdouandoudms@gmail.com")
 # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@nutritionapp.com")
+# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@nutritionapp.com")
 FRONTEND_URL = "http://localhost:5173"
 AI_SERVICE_PREDICT_URL = os.getenv(
     "AI_SERVICE_PREDICT_URL", 
